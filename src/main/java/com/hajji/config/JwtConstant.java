0a21d88e6411099d83e64fcf1612a57037fe55b4
naderhaji <nader9hajji@gmail.com>
2025-01-31 10:33:04 +0100

    Authentification User

---
+++ src/main/java/com/hajji/config/JwtConstant.java
@@ -0,0 +1,10 @@
+package com.hajji.config;
+
+public class JwtConstant {
+
+    public static final String JWT_SECRET= "ffffrffrefergeazeadxedfsvcrcwdczferevetrvr";
+
+    public static final String JWT_HEADER= "Authorization";
+
+
+}
